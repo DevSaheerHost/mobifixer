2d7f5cf9f594913db0c418d5b2b13d0a7246ed0c
devsaheerhost <sheerbabu549@gmail.com>
2026-01-06 21:11:41 +0530

    ui ux fully setted in user : settings page and sign out function

--- cashbook/main.js
+++ cashbook/main.js
@@ -403,7 +403,55 @@ firebase.database().ref(`/users/${username}`).get()
 
         authView.style.display='none';
         mainView.style.display='block';
-        userArea.innerHTML = `<div style="display:flex;gap:8px;align-items:center"><div>${user.email}</div><button id='signOut' class='link'>Sign out</button></div>`;
+        
+        userArea.innerHTML = `
+        <details class="user-menu">
+  <summary class="user-summary">
+    <span class="username">${fullname}</span>
+
+    
+    
+  </summary>
+
+  <div class="menu-content" style='color: #fff'>
+    <div style='border-bottom: 1px solid #EFEFEF; padding: 0.5rem; padding-top: 0;'>
+      <p style='color: #000; font-weight: 500; font-size: 18px; margin:0;'>${fullname}</p>
+    </div>
+    <button class="link hidden">Profile</button>
+    
+    <button class="link" style='background: rgba(11, 162, 255, 0.15);' data-link='settings'>
+      <div class='flex'>
+        
+              <svg style='width: 20px ;min-width: 20px; color: var(--accent); font-weight: bold;' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
+
+
+
+
+
+Settings
+      </div>
+
+
+
+<svg style='height: 20px; color: var(--accent)' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+</svg>
+
+
+    </button>
+    <button class="link danger signout" id='signOut'>
+                          <svg style='width: 20px' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+</svg>
+Sign out</button>
+  </div>
+</details>
+        `
+       
         document.getElementById('signOut').addEventListener('click', async ()=>{
         
           const userConfirmed = await askUserPermission({
@@ -2727,3 +2775,52 @@ if (isBirthday) {
     setTimeout(() => el.remove(), 4000);
   }, 400); // 👈 slow, classy
 }
+
+
+
+
+// page switching 
+
+const PageRouter = (() => {
+  const pages = document.querySelectorAll('.page');
+  let current = document.querySelector('.page.active');
+
+  function show(hash, from = 'direct') {
+    const id = hash.replace('#', '') || 'home';
+    const next = document.getElementById(id);
+
+    if (!next || next === current) return;
+
+    // exit current
+    current.classList.remove('active');
+    current.classList.add('exit');
+
+    // enter next
+    next.classList.remove('exit');
+    next.classList.add('active');
+
+    current = next;
+  }
+
+  // click handler
+  document.addEventListener('click', e => {
+    const link = e.target.closest('[data-link]');
+    if (!link) return;
+
+    e.preventDefault();
+    location.hash = link.dataset.link;
+  });
+
+  // hash change (back / forward)
+  window.addEventListener('hashchange', () => {
+    show(location.hash, 'back');
+  });
+
+  // first load
+  show(location.hash);
+
+  return { show };
+})();
+
+//PageRouter.show('#about');
+//PageRouter.show('#settings');
